1f57ee9041f23ecbff38d93a81d37bca363e81d1
uday-arya <udayarya0704@gmail.com>
2025-11-10 23:29:09 -0500

    first commit

---
+++ src/main/java/com/SYSC3005/Main/A3q1Application.java
@@ -0,0 +1,13 @@
+package com.SYSC3005.Main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A3q1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A3q1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/SYSC3005/Main/A3q1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SYSC3005.Main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A3q1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
